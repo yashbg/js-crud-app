87281f4f4aa1264c9ab77d31b42a8275d4af4aba
Yash Gupta <yashbg7@gmail.com>
2022-03-15 00:06:37 +0530

    Add crud functions

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-'use strict'
+'use strict';
 
+let addressMap = new Map();
+
+function addUser(name, address){
+    addressMap.set(name, address);
+}
+
+function getAddress(name){
+    return addressMap.get(name);
+}
+
+function updateAddress(name, newAddress){
+    addressMap.set(name, newAddress);
+}
+
+function deleteUser(name){
+    addressMap.delete(name);
+}
