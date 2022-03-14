3507b456462d2103a2b512c2abd1764697ae6f93
Yash Gupta <yashbg7@gmail.com>
2022-03-15 00:52:15 +0530

    Add html and remove function params

--- script.js
+++ script.js
@@ -2,38 +2,68 @@
 
 let addressMap = new Map();
 
-function addUser(name, address){
-    if(addressMap.has(name)){
+function addUser() {
+    let name = prompt('Name:', '');
+    if (!name) {
+        alert(`You didn't provide the name.`);
+        return;
+    }
+    if (addressMap.has(name)) {
         alert(`User ${name} already exists.`);
+        return;
+    }
+    let address = prompt('Address:', '');
+    if (!address) {
+        alert(`You didn't provide the address.`);
         return;
     }
     addressMap.set(name, address);
     alert(`User ${name} added.`)
 }
 
-function getAddress(name){
-    if(!addressMap.has(name)){
+function getAddress() {
+    let name = prompt('Name:', '');
+    if (!name) {
+        alert(`You didn't provide the name.`);
+        return;
+    }
+    if (!addressMap.has(name)) {
         alert(`User ${name} doesn't exist.`);
         return;
     }
-    address =  addressMap.get(name);
-    alert(`Address of ${name} is ${address}`);
+    let address = addressMap.get(name);
+    alert(`Address of ${name} is ${address}.`);
 }
 
-function updateAddress(name, newAddress){
-    if(!addressMap.has(name)){
+function updateAddress() {
+    let name = prompt('Name:', '');
+    if (!name) {
+        alert(`You didn't provide the name.`);
+        return;
+    }
+    if (!addressMap.has(name)) {
         alert(`User ${name} doesn't exist.`);
+        return;
+    }
+    let newAddress = prompt('New Address:', '');
+    if (!newAddress) {
+        alert(`You didn't provide the new address.`);
         return;
     }
     addressMap.set(name, newAddress);
     alert(`Address of ${name} updated.`);
 }
 
-function deleteUser(name){
-    if(!addressMap.has(name)){
+function deleteUser() {
+    let name = prompt('Name:', '');
+    if (!name) {
+        alert(`You didn't provide the name.`);
+        return;
+    }
+    if (!addressMap.has(name)) {
         alert(`User ${name} doesn't exist.`);
         return;
     }
     addressMap.delete(name);
-    alert(`User ${name} deleted`);
+    alert(`User ${name} deleted.`);
 }
