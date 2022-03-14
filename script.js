64dbc7538bfe0be86935117ce290f2091e7e6260
Yash Gupta <yashbg7@gmail.com>
2022-03-15 00:14:03 +0530

    Check existence of user and alert instead of return

--- script.js
+++ script.js
@@ -3,17 +3,37 @@
 let addressMap = new Map();
 
 function addUser(name, address){
+    if(addressMap.has(name)){
+        alert(`User ${name} already exists.`);
+        return;
+    }
     addressMap.set(name, address);
+    alert(`User ${name} added.`)
 }
 
 function getAddress(name){
-    return addressMap.get(name);
+    if(!addressMap.has(name)){
+        alert(`User ${name} doesn't exist.`);
+        return;
+    }
+    address =  addressMap.get(name);
+    alert(`Address of ${name} is ${address}`);
 }
 
 function updateAddress(name, newAddress){
+    if(!addressMap.has(name)){
+        alert(`User ${name} doesn't exist.`);
+        return;
+    }
     addressMap.set(name, newAddress);
+    alert(`Address of ${name} updated.`);
 }
 
 function deleteUser(name){
+    if(!addressMap.has(name)){
+        alert(`User ${name} doesn't exist.`);
+        return;
+    }
     addressMap.delete(name);
+    alert(`User ${name} deleted`);
 }
